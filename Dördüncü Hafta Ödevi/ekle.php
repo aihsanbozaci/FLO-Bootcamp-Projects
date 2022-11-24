52a21805a3d1987dc353fa7bf2e445160ceec905
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-11-24 21:20:54 +0300

    Dördüncü Hafta Ödevi

--- Dördüncü Hafta Ödevi/ekle.php
+++ Dördüncü Hafta Ödevi/ekle.php
@@ -1,5 +1,6 @@
 <?php
 require("fonksiyonlar.php");
+$baglan = new Veritabani("localhost", "root", "aabbcc123", "dorduncuhafta");
 if(isset($_POST['kaydet'])){
     $adsoyad = $_POST['adsoyad'];
     $tcno = $_POST['tcno'];
