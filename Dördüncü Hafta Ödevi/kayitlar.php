66fe403418a34977e142350480935a2738473e03
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-11-26 14:43:15 +0300

    Dördüncü Hafta Ödevi

--- Dördüncü Hafta Ödevi/kayitlar.php
+++ Dördüncü Hafta Ödevi/kayitlar.php
@@ -26,7 +26,7 @@ class Kayitlar
         $islem1 = (((($this->tcno[0] + $this->tcno[2] + $this->tcno[4] + $this->tcno[6] + $this->tcno[8]) * 7) - ($this->tcno[1] + $this->tcno[3] + $this->tcno[5] + $this->tcno[7]))) % 10 == $this->tcno[9];
         $islem2 = ($this->tcno[0] + $this->tcno[1] + $this->tcno[2] + $this->tcno[3] + $this->tcno[4] + $this->tcno[5] + $this->tcno[6] + $this->tcno[7] + $this->tcno[8] + $this->tcno[9] + $this->tcno[10]) % 10 == $this->tcno[10];
 
-        if (is_numeric($this->tcno) && strlen($this->tcno) == 11 && substr($this->tcno, 0, 1) != 0 && $islem1 && $islem2) {
+        if (filter_var($this->tcno, FILTER_VALIDATE_INT) && strlen($this->tcno) == 11 && substr($this->tcno, 0, 1) != 0 && $islem1 && $islem2) {
             return "T.C Kimlik Geçerli";
         } else {
             return "T.C Kimlik Geçersiz";
