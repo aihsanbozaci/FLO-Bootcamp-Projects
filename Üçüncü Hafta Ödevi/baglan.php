33d48db62f752bd88edd29299279e6bfe0f04d38
Ahmet İhsan Bozacı <ahmetbozac@gmail.com>
2022-12-13 18:43:48 +0300

    Update baglan.php

--- Üçüncü Hafta Ödevi/baglan.php
+++ Üçüncü Hafta Ödevi/baglan.php
@@ -1,7 +1,7 @@
 <?php
 $mysqlsunucu = "localhost";
 $mysqlkullanici = "root";
-$mysqlsifre = "aabbcc123";
+$mysqlsifre = "";
 $veritabani = "thirdweekproject";
 try {
     $baglan = new PDO("mysql:host=$mysqlsunucu;dbname=$veritabani;charset=utf8", $mysqlkullanici, $mysqlsifre);
